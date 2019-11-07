28ea7c76e29f865894218bbcaafa5065169efea5
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2019-11-07 14:44:52 -0800

    Full app

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let randomNum = Math.floor(Math.random() * 7)
+
+const days = [
+  {japanese:"にちようび", english:"sunday"},
+  {japanese:"げつようび", english:"monday"},
+  {japanese:"かようび", english:"tuesday"},
+  {japanese:"すいようび", english:"wednesday"},
+  {japanese:"もくようび", english:"thursday"},
+  {japanese:"きんようび", english:"friday"},
+  {japanese:"どようび", english:"saturday"}
+];
+
+function eval(){
+  event.preventDefault();
+  if(document.querySelector("#input").value.toLowerCase() === days[randomNum].english) {
+    document.querySelector("#day").textContent = "Correct!"
+    document.querySelector("#day").classList.add("correct")
+  } else {
+    document.querySelector("#day").textContent = "Incorrect!"
+    document.querySelector("#day").classList.add("incorrect")
+  }
+  setTimeout(function(){location.reload();}, 1000);
+}
+
+document.querySelector("#day").textContent = days[randomNum].japanese;
+document.querySelector("#button").addEventListener("click", () => eval());
+document.addEventListener("submit", () => eval());
